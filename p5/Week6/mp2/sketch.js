5500e8505a8d6abf6e7a3ca605d07627d75580ec
mdaciolas <54365372+mdaciolas@users.noreply.github.com>
2020-02-20 11:15:30 -0600

    add

---
+++ p5/Week6/mp2/sketch.js
@@ -0,0 +1,54 @@
+var myState = 0;
+var timer = 0;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+}
+
+function draw() {
+  // put drawing code here
+
+  switch(myState){
+    case 0:
+    background('red');
+    text("Beg. of story", 100, 100);
+    timer++ ;
+    if (timer > 200){
+      myState = 1;
+      timer = 0;
+    }
+    break;
+
+    case 1:
+    background('blue');
+    text("1st part", 100, 100);
+    break;
+
+    case 2:
+    background('green');
+    text("2nd part", 100, 100);
+    break;
+
+    case 3:
+    background('yellow');
+    text("3rd part", 100, 100);
+    break;
+
+    case 4:
+    background('orange');
+    text("finale", 100, 100);
+    break;
+
+  }
+
+}
+
+
+
+function mouseReleased() {
+  myState = myState + 1;
+  if(myState > 4){
+    myState = 0;
+  }
+}
