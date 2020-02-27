606093a88e6b0ff3c01098a8f19748660bbece86
mdaciolas <54365372+mdaciolas@users.noreply.github.com>
2020-02-27 10:36:22 -0600

    week7

--- p5/Week6/mp2/sketch.js
+++ p5/Week6/mp2/sketch.js
@@ -2,10 +2,16 @@ var myState = 0;
 var timer = 0;
 var x= 2;
 var y= 2;
+var myFont;
+
+
+function preload(){
+  myFont= loadFont('assets/font.ttf')
+}
 
 function setup() {
   // put setup code here
-  createCanvas(800, 800);
+  createCanvas(1000, 800);
 }
 
 function draw() {
@@ -15,9 +21,10 @@ function draw() {
     case 0:
     background('red');
     fill(0,0,0);
-    textSize(25);
+    textSize(50);
+    textFont(myFont);
     text("this is the story of", 10, 100);
-    text("finding a hamster in my room", 10, 130);
+    text("finding a hamster in my room", 10, 150);
     timer++ ;
     if (timer > 200){
       myState = 1;
@@ -30,8 +37,26 @@ function draw() {
     background('blue');
     fill(0,0,0);
     text("based on true events", 10, 100);
+
     fill(255,255,255);
+    //ears
+    ellipse(300,300, 10, 40);
+    ellipse(400,300, 10, 40);
+    //body
     ellipse(350,437,250,300);
+
+    //eyes
+    fill(0,0,0);
+    ellipse(300, 350, 10, 10);
+    ellipse(400, 350, 10, 10);
+
+    //mouth
+    fill(255, 102, 235);
+    triangle(325, 370, 375, 370, 350, 390);
+
+    //floor
+    fill(143, 88, 0);
+    rect(0, 600, 1000, 800);
     break;
 
     case 2:
@@ -40,6 +65,15 @@ function draw() {
     text("He run", 100, 100);
     fill(255,255,255);
     ellipse(x ,437, 250, 300);
+
+    //fill(0,0,0);
+    hamster(x , 350, 10, 10);
+  //  ellipse(x, 350, 10, 10);
+    //fill(255, 102, 235);
+    //triangle(325, 370, 375, 370, 350, 390);
+
+    fill(143, 88, 0);
+    rect(0, 600, 1000, 800);
     x = x + 5;
 
     if (x > width){
@@ -48,11 +82,16 @@ function draw() {
     break;
 
     case 3:
-    background('yellow');
-    fill(0,0,0);
-    text("He ride the elevator", 100, 100);
+    background('black');
+    fill(110, 81, 1);
+    rect(200 , 0 , 600, 800);
     fill(255,255,255);
-    ellipse(400, y, 250, 300);
+    ellipse(500, y, 250, 300);
+    //fill(0,0,0);
+    //ellipse(500, 350, 10, 10);
+    //ellipse(600, 350, 10, 10);
+    fill(255,255,255);
+    text("He ride the elevator", 100, 100);
     y = y + 5;
     if (y > height){
       y=0;
@@ -64,15 +103,35 @@ function draw() {
     case 4:
     background('orange');
     fill(0,0,0);
-    text("He scare", 100, 100);
+    text("He hid in trash", 100, 100);
+    text("but he can't escape", 100, 200);
+
     fill(255,255,255);
     ellipse(400, 437, 250, 300);
+
+    fill(0,0,0);
+    ellipse(350, 350, 10, 10);
+    ellipse(450, 350, 10, 10);
+
+    fill(255, 102, 235);
+    triangle(375, 370, 425, 370, 395, 390);
+
+    fill(46, 44, 46);
+    square(215, 500, 420);
+    fill(0,0,0);
+    ellipse(425, 500, 500, 50);
     break;
 
   }
-
 }
 
+function hamster(){
+  fill(0,0,0);
+  ellipse(300 , 350, 10, 10);
+  ellipse(400, 350, 10, 10);
+  fill(255, 102, 235);
+  triangle(325, 370, 375, 370, 350, 390);
+}
 
 
 function mouseReleased() {
